db1a3c41adfee033e3d2e106758ed16e7ed5f335
ChenXuYangJP <xycheninjp@yahoo.co.jp>
2026-09-19 22:10:33 +0900

    Initial UE 5.8 CGHSim project

---
+++ Source/CGHSim/CGHSim.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CGHSim.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CGHSim, "CGHSim" );
